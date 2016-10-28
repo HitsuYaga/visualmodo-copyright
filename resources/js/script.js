285acebce64c6a7bdf175358a6272a7dda091c2a
HitsuYaga <dq.tho89@gmail.com>
2016-10-28 16:06:26 +0700

    finished customer services

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,4 @@
 /* HOVER MENU DROPDOWN */
-<<<<<<< HEAD
 var menudropdown = $('.js-dropdown .menu-dropdown');
 $('.js--dropdown').hover(
     function() {
@@ -9,27 +8,12 @@ $('.js--dropdown').hover(
     function() {
         $(this).parent().find(menudropdown).removeClass('menu-dropdown-visible fadeInUp');
         $(this).parent().find(menudropdown).addClass('fadeOutDown');
-=======
-var menudropdown = $('.js--dropdown .menu-dropdown');
-$('.js--dropdown').hover(
-    function() {
-        $(this).find(menudropdown).removeClass('fadeOutDown');
-        $(this).find(menudropdown).addClass('menu-dropdown-visible animated fadeInUp');
-    },
-    function() {
-        $(this).find(menudropdown).removeClass('menu-dropdown-visible fadeInUp');
-        $(this).find(menudropdown).addClass('fadeOutDown');
->>>>>>> origin/master
     }
-);
+)
 
 $('.menu-dropdown').hover(
     function() {
-<<<<<<< HEAD
-        $(this).addClass('menu-dropdown-visible')
-=======
         $(this).addClass('menu-dropdown-visible');
->>>>>>> origin/master
         $(this).removeClass('fadeOutDown');
     },
     function() {
@@ -39,13 +23,9 @@ $('.menu-dropdown').hover(
 );
 
 /* HOVER DESIGN PHOTO LIST */
-<<<<<<< HEAD
 var designphoto = $('.design-photo-list .design-photo');
 var designphotodes = $('.design-photo-list .design-photo-des');
-=======
-var $designphoto = $('.design-photo-list .design-photo');
-var $designphotodes = $('.design-photo-list .design-photo-des');
->>>>>>> origin/master
+
 $('.design-photo-list').hover (
     function(){
         $(this).find(designphoto).toggleClass('design-photo-hover');
